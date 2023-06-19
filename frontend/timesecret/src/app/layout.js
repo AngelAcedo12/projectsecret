de064c0dab6e76eeff73073d263fef9d29d63bab
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2023-06-19 16:47:13 +0200

    m

--- frontend/timesecret/src/app/layout.js
+++ frontend/timesecret/src/app/layout.js
@@ -1,6 +1,5 @@
-import { Inter } from 'next/font/google'
-
 import { Analytics } from '@vercel/analytics/react'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
